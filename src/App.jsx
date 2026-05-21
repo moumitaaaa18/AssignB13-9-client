819bbba48f81a6892370b0daba3549bb57acca9f
moumitaaaa18 <dharchowdhurymoumita812@gmail.com>
2026-05-22 00:24:57 +0600

    new appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AvailableCars from "./pages/AvailableCars";
 import CarDetails from "./pages/CarDetails";
 import AddCar from "./pages/AddCar";
 import MyCars from "./pages/MyCars";
+import UpdateCar from "./pages/UpdateCar";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="cars/:id" element={<CarDetails />} />
         <Route path="add-car" element={<AddCar />} />
         <Route path="my-cars" element={<MyCars />} />
+        <Route path="update-car/:id" element={<UpdateCar />} />
       </Route>
     </Routes>
   );
